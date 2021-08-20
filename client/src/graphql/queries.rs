f5e0bf3633febc205cfa56c2548e37a7ff30d8e1
Erik Tesar <erik@erik-tesar.com>
2021-08-20 18:19:30 +0200

    refactor: write better query for getChallenge

--- client/src/graphql/queries.rs
+++ client/src/graphql/queries.rs
@@ -1,9 +1,29 @@
-use graphql_client::GraphQLQuery;
+use graphql_client::{GraphQLQuery, QueryBody};
 
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "../other/graphql/schema.graphql",
-    query_path = "../other/graphql/queries.graphql",
-    response_derives = "Debug"
-)]
-pub(crate) struct GetChallenge;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[non_exhaustive]
+/// A GraphQL query to get a challenge used for signup
+pub struct GetChallenge {
+    // since the schema for this query is
+    // ```graphql
+    // getChallenge: String!
+    // ```
+    // it returns ``{ "getChallenge": "xxx" } ``, which doesn't look so good, just ``challenge`` looks better imo
+    #[serde(rename = "getChallenge")]
+    pub challenge: String,
+}
+
+impl GraphQLQuery for GetChallenge {
+    // we have no variables
+    type Variables = ();
+    type ResponseData = Self;
+    fn build_query(_: Self::Variables) -> QueryBody<Self::Variables> {
+        QueryBody {
+            variables: (),
+            query: include_str!("../../../other/graphql/queries/get_challenge.graphql"),
+            operation_name: "getChallenge",
+        }
+    }
+}
